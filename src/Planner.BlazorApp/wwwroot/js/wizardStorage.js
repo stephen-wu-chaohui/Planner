87d448e0485d0902c4036f39955d384516b9da7c
stephen-wu-chaohui <stephen.wu.chaohui@gmail.com>
2025-11-15 15:58:39 +0800

    Polishing on UI

---
+++ src/Planner.BlazorApp/wwwroot/js/wizardStorage.js
@@ -0,0 +1,8 @@
+﻿window.wizardStorage = {
+    setSeen: function () {
+        localStorage.setItem("planner-wizard-seen", "1");
+    },
+    hasSeen: function () {
+        return localStorage.getItem("planner-wizard-seen") === "1";
+    }
+};
