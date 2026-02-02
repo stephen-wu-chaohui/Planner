34c900a6940e8aeba7b3f368ca6c178a43358b8c
stephen-wu-chaohui <stephen.wu.chaohui@gmail.com>
2026-02-02 22:23:00 +0800

    Fix test cases

--- test/Planner.API.EndToEndTests/OptimizationController.EndToEndTests.cs
+++ test/Planner.API.EndToEndTests/OptimizationController.EndToEndTests.cs
@@ -43,7 +43,7 @@ public sealed class OptimizationControllerEndToEndTests {
 
         method.Should().NotBeNull();
 
-        var request = await (Task<OptimizeRouteRequest>) method!.Invoke(controller, null)!;
+        var request = await (Task<OptimizeRouteRequest>) method!.Invoke(controller, [null])!;
 
         request.Should().NotBeNull();
 
